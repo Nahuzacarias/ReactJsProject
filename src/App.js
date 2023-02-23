535a2bb684697335a40dc139993fe9f17741817d
Nahuzacarias <zacarias.nahu@gmail.com>
2023-02-23 12:44:48 -0300

    map

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Home from "./Components/Pages/Home/Home"
 import Contact from "./Components/Pages/Contact/Contact"
 import About from "./Components/Pages/About/About"
 import CharacterDetail from './Components/Pages/CharacterDetail/CharacterDetail';
-import productos from './Components/productos/productos';
 
 const App = () => {
   return (
@@ -18,14 +17,7 @@ const App = () => {
         <Itemlistcontainer/>
 
 
-        {
 
-productos.map((prod) => (
-  <productos id={prod.id} nombre={prod.nombre} precio={prod.precio} foto={prod.foto} descripcion= {prod.descripcion} />
-  
-  ))
-
-}
         <Routes>
 
         <Route path='/about' element={<About />} />
